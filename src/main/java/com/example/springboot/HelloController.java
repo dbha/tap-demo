9bfe9ffced88f16b445af731b56a1bb31b28e529
dbha <dbha0719@gmail.com>
2022-05-09 12:57:14 +0900

    update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello TAP multi-cluster kapp action";
+		return "Hello TAP multi-cluster git-ops";
 	}
 
 }
